62e3db51ca0156a9f177b355589f1fd8f5ac619e
Babalitor <leblanc.bastien2@gmail.com>
2025-02-18 16:46:42 +0100

    update players.py
    adding play() method

--- games/torches/Players/players.py
+++ games/torches/Players/players.py
@@ -1,8 +1,8 @@
 """
 Module qui gère les différents types de joueurs
 """
-
 import random
+from Game.game_model import *
 
 # --- Player Classes ---
 class Player:
@@ -70,10 +70,42 @@ class Human(Player):
     
 
 class ia_player(Player):
-    def __init__(self, epsilon = 0.9, learning_rate = 0.01, historique =None, previous_state = None):
-        super().__init__()
+    """
+    Classe qui représente un player AI (qui hérite de player)
+    redéfini play 
+    """
+    def __init__(self,name:str, game=None, epsilon: float = 0.9, learning_rate: float= 0.01, historique: list[int]=None, previous_state: int= None)->None:
+        super().__init__(name,game)
         self.eps= epsilon
         self.lr = learning_rate
         self.historique = [] or historique
         self.previous_state = previous_state
         self.v_function = {"lose":-1, "win":1}
+
+    def exploit():
+        pass
+
+    def play(self)->int:
+        """
+        Ajoute la transition des coups à l'historique,
+        en fonction de l'epsilon, joue soit un nb aleatoire,
+        soit le coup réfléchi par l'exploitation.
+        met à jour l'état précédent
+        
+        Returns:
+            -Int : le coup à jouer choisi par l'ia
+        """
+        if self.previous_state:
+            self.historique.append((self.previous_state, GameModel.nb))
+
+        #eps = proba qui choisi si on exploite ou si on explore (random)
+        if random.uniform(0,1.0) < self.eps:
+            move = random.randint(1,3)
+        else:
+            move = self.exploit()
+
+        self.previous_state = GameModel.nb - move
+        return move
+    
+    def win(self)->None:
+        pass
