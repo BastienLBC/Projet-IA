78cf6ef149781e6f95742a7d89e47f1d457f7434
Hug00at <etu52503@henallux.be>
2025-02-25 12:22:04 +0100

    Update Alumettes.py

--- games/torches/Alumettes.py
+++ games/torches/Alumettes.py
@@ -1,53 +1,6 @@
 from Game.game_controller import *
 from Players.players import Human, ia_player,Player
 
-def training(ai1, ai2, nb_games, nb_epsilon):
-        """
-        Train the AIs @ai1 and @ai2 during @nb_games games
-        epsilon decrease every @nb_epsilon games
-        """
-        training_game = GameModel(12, ai1, ai2, displayable=False)
-        for i in range(nb_games):
-            if i % nb_epsilon == 0:
-                if isinstance(ai1, ia_player):
-                    ai1.next_epsilon()
-                if isinstance(ai2, ia_player):
-                    ai2.next_epsilon()
-    
-            training_game.play()
-            if isinstance(ai1, ia_player):
-                ai1.train()
-            if isinstance(ai2, ia_player):
-                ai2.train()
-    
-            training_game.reset()
-    
-def compare_ai(*ais):
-        """
-        Print a comparison between the @ais
-        """
-        names = f"{'':4}"
-        stats1 = f"{'':4}"
-        stats2 = f"{'':4}"
-    
-        for ai in ais:
-            names += f"{ai.name:^15}"
-            stats1 += f"{str(ai.wins)+'/'+str(ai.nb_games):^15}"
-            stats2 += f"{f'{ai.wins/ai.nb_games*100:4.4}'+'%':^15}"
-    
-        print(names)
-        print(stats1)
-        print(stats2)
-        print(f"{'-'*4}{'-'*len(ais)*15}")
-    
-        all_v_dict = {key: [ai.v_function.get(key, 0) for ai in ais] for key in ais[0].v_function.keys()}
-        sorted_v = lambda v_dict: sorted(filter(lambda x: isinstance(x[0], int), v_dict.items()))
-        for state, values in sorted_v(all_v_dict):
-            print(f"{state:2} :", end='')
-            for value in values:
-                print(f"{value:^15.3}", end='')
-            print()
-
 if __name__ == "__main__":
     joueur = Player('Joueur')
     alice = ia_player('alice')
@@ -56,10 +9,10 @@ if __name__ == "__main__":
 
 
     # Entraînement de Alice et Bob
-    training([alice, bob], 1000, 10)
+    training(alice, bob, 1000, 10)
 
     # Entraînement de Randy contre le joueur basique
-    training([randy, joueur], 1000, 10)
+    training(randy, joueur, 1000, 10)
     """"
     # Remettre les stats de Bob à 0
     reset_stats(bob)
