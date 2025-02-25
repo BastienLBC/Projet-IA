69a0ce8c33b803c495ee11b0394f3498efcea5b0
Hug00at <etu52503@henallux.be>
2025-02-25 10:42:52 +0100

    Update players.py

--- games/torches/Players/players.py
+++ games/torches/Players/players.py
@@ -3,6 +3,7 @@ Module qui gère les différents types de joueurs
 """
 
 import random
+from Game.game_controller import *
 
 # --- Player Classes ---
 class Player:
@@ -68,7 +69,6 @@ class Human(Player):
     def play(self) -> int:
         return int(input(f"{self.name}, combien d'allumettes prenez-vous (1-3) ? "))
     
-
 class ia_player(Player):
     def __init__(self, epsilon = 0.9, learning_rate = 0.01, historique =None, previous_state = None):
         super().__init__()
@@ -77,3 +77,52 @@ class ia_player(Player):
         self.historique = [] or historique
         self.previous_state = previous_state
         self.v_function = {"lose":-1, "win":1}
+
+
+def exploit(self) -> int:
+    """
+    choisit la pire action possible pour le donner a son adversaire
+    """
+    best_move = None
+    best_value = 1
+    nb_torchs = self.game.nb
+
+    for i in [2, 3]:
+        next_state = nb_torchs - i
+        next_value = self.v_function.get(next_state, 0)
+
+        if next_value < best_value:
+            best_value = next_value
+            best_move = i
+
+    return best_move
+
+def play(self):
+    if self.previous_state is not None:
+        self.historique.append((self.previous_state, self.nb_torchs))
+
+    self.previous_state = self.nb_torchs
+
+    return self.exploit()
+
+def win(self) -> None:
+    """
+    Ajoute une victoire au nb de victoires,
+    ajoute la dernière transition avec win à l'historique, 
+    remets previous_state à None.
+    """
+    super().win()
+    if self.previous_state is not None:
+        self.historique.append((self.previous_state, "win"))
+    self.previous_state = None
+
+def lose(self) -> None:
+    """
+    Ajoute une défaite au nb de défaites,
+    ajoute la dernière transition avec lose à l'historique,
+    remets previous_state à None.
+    """
+    super().lose()
+    if self.previous_state is not None:
+        self.historique.append((self.previous_state, "lose"))
+    self.previous_state = None
