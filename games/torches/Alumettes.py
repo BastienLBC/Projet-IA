278738cdebf820ddd2670bb7c701737425bea176
Hug00at <etu52503@henallux.be>
2025-02-27 00:33:28 +0100

    Update Alumettes.py

--- games/torches/Alumettes.py
+++ games/torches/Alumettes.py
@@ -53,20 +53,4 @@ if __name__ == "__main__":
     # Entraînement de Randy contre le joueur basique
     training([randy, joueur], 1000, 10)
 
-    """"
-    # Remettre les stats de Bob à 0
-    reset_stats(bob)
-
-    # Faire jouer Bob contre le joueur basique
-    training([bob, joueur], 1000, 10)
-
-    # Comparer les stats et la value-function des IA
-    compare_stats_and_value_function([alice, bob, randy])
-
-    # Relancer les mêmes tests avec 100.000 parties
-    train([alice, bob], 100000, 10)
-    train([randy, joueur], 100000, 10)
-    reset_stats(bob)
-    train([bob, joueur], 100000, 10)
-    compare_stats_and_value_function([alice, bob, randy])
-    """
+  
