cf89b67f5c53eb6c61f374e8ca003755f0abe69f
Hug00at <etu52503@henallux.be>
2025-02-27 04:31:24 +0100

    Update game_model.py

--- games/torches/Game/game_model.py
+++ games/torches/Game/game_model.py
@@ -53,7 +53,7 @@ class GameModel:
         """
         affiche le nombre d'alumettes restantes dans la console
         """
-        if self.display:
+        if self.display == True:
             print(f"Allumettes restantes: {self.nb}")
 
     def step(self, action:int)->None:
