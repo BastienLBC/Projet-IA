2f70ba3b0caa96d886f9934213b5723a771b6976
Babalitor <leblanc.bastien2@gmail.com>
2025-03-17 19:26:07 +0100

    view update

--- games/Cube/game_view.py
+++ games/Cube/game_view.py
@@ -1,14 +1,31 @@
+"""
+Vue du jeu cubee
+Règles : But :
+
+Posséder plus de cases que l’adversaire
+
+Déroulement :
+
+Deux joueurs s'affrontent sur un plateau carré d'une taille variable (5x5 cases par défaut).
+
+Un joueur gagne des cases en se déplaçant dessus.
+
+Au début de la partie, les joueurs commencent chacun dans un coin opposé du terrain. Chacun à leur tour, ils vont se déplacer d'une case (vers le haut, le bas, la gauche ou la droite). Un joueur ne peut pas se déplacer sur une case appartenant au joueur adverse ni sortir des limites du plateau.
+"""
+
 import customtkinter as ctk
+
+from game_controller import GameController
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
 
 class GameView(ctk.CTk):
-    def __init__(self, controller)->None:
+    def __init__(self,controller)->None:
         """
         initialisation de gameView
         Args:
-            controller; instance de GameController
+            controller:
         """
         super().__init__()
         self.controller = controller
@@ -30,8 +47,21 @@ class GameView(ctk.CTk):
 
         self.update_view()
 
+    def draw_board(self)->None:
+        """
+        créateur du tableau en colorant les cases du plateau où les joueurs sont passés (dans la couleur du joueur(Un des joueurs a le vert et l'autre gris clair))
+        """
+
+
     def update_view(self)->None:
         """
-        Met à jour l'écran en supprimant et remettant le nouveau tableau
+        Met à jour la vue et le tableau
         """
+
+
+    def reset(self):
         pass
+
+    def end_game(self):
+        pass
+
