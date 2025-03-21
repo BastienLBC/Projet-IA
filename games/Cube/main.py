db60e41a17265849fde5891ead09b2b377320474
Babalitor <leblanc.bastien2@gmail.com>
2025-03-21 09:33:50 +0100

    menu alumettes

--- games/Cube/main.py
+++ games/Cube/main.py
@@ -2,7 +2,7 @@ from Game.game_controller import *
 
 def test_display():
     # Créez deux joueurs
-    player1 = Player(name="Player 1", color="red")
+    player1 = Player(name="Player 1", color="grey")
     player2 = HumanPlayer(name="Player 2", color="green")
 
     # Initialisez le contrôleur avec les joueurs
