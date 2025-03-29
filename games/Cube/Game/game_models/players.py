bf21fd229f305030da25cbbcbe27815b51d559e4
Babalitor <leblanc.bastien2@gmail.com>
2025-03-29 18:26:06 +0100

    players ia

--- games/Cube/Game/game_models/players.py
+++ games/Cube/Game/game_models/players.py
@@ -66,8 +66,12 @@ class AiPlayer(Player):
     def reward(self, old_state:tuple, new_state:tuple):
         """
         calcule la récompense en fonction de si il prend une case, enclo ou si il bloque l'adversaire
+        +1 : prise case blanche
+        +5 : creation d'enclo
+        -0.5 : par nb de cases blanches autour de l'adversaire (pour le forcer à enfermer l'adversaire)
+        +-20 : victoire/ defaite
         Returns:
-            int: reaward
+            float: reaward
         """
         reward = 0
         ennemy = self.game.players1 if self == self.game.players2 else self.game.players2
@@ -75,11 +79,41 @@ class AiPlayer(Player):
         if self.game.matrix[new_state[0]][new_state[1]]["color"] == "white":
             reward += 1
 
-    def creates_enclosure(self, x: int, y: int) -> bool:
+        if self.is_enclosure(new_state[0], new_state[1]):
+            reward += 5
+
+        accessibles_cases = self.accessible_cases(ennemy)
+        reward -= accessibles_cases * 0.5
+
+        if self.game.is_finished():
+            if self.game.get_winner() == self:
+                reward += 20
+            else:
+                reward -= 20
+
+        return reward
+
+    def accessible_cases(self,player):
+        """
+        compte le nb de cases blanches autour d'un joueur
+        Args:
+            player:
+        Returns:
+            int : nb de cases blanches autour du player
+        """
+        count = 0
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            new_x, new_y = player.x + dx, player.y + dy
+            if (0 <= new_x < self.game.board and 0 <= new_y < self.game.board and
+                    self.game.matrix[new_x][new_y]["color"] == "white"):
+                count += 1
+        return count
+
+    def is_enclosure(self, x: int, y: int) -> bool:
         """
         verifie si le déplacement crée un enclo
         Returns:
-            bool: True si le mouvement crée un enclos
+            bool: True si le mouvement crée un encloada
         """
         reachable = [[False for _ in range(self.board)] for _ in range(self.board)]
         queue = [(x, y)]
@@ -97,3 +131,20 @@ class AiPlayer(Player):
         return any(self.matrix[i][j]["color"] == "white" and not reachable[i][j]
                    for i in range(self.board)
                    for j in range(self.board))
+
+    def play(self) -> str:
+        """
+        return le bind choisi par l'ia, explore ou exploite et initialise l'état si pas encore fait
+        Returns:
+            str : bind
+        """""
+        state = self.get_state()
+        if state not in self.q_table:
+            self.q_table[state] = {
+                "UP": 0, "DOWN": 0, "LEFT": 0, "RIGHT": 0
+            }
+
+        if random.random() < self.eps:
+            return random.choice(["UP", "DOWN", "LEFT", "RIGHT"])
+        else:
+            return max(self.q_table[state].items(), key=lambda x: x[1])[0]
