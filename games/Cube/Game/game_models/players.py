fb065e812bdb010cfae78ea27882d27b7baa1737
Hug00at <etu52503@henallux.be>
2025-04-05 10:52:20 +0200

    Update players.py

--- games/Cube/Game/game_models/players.py
+++ games/Cube/Game/game_models/players.py
@@ -9,7 +9,6 @@ class Player:
         self.color = color
         self.win = 0
         self.losses = 0
-        
         self.x = 0
         self.y = 0
 
@@ -55,96 +54,40 @@ class AiPlayer(Player):
         self.eps = epsilon #exploration
         self.q_table = {}
 
+    def reward(self):
+       
+        ennemy = self.game.players1 if self == self.game.players2 else self.game.players2
+        point_ennemy = 1
+        
+        if ennemy.point > point_ennemy:
+            point_ennemy == ennemy.point
+            self.reward -= 1
+            ennemy.reward += 1
+        return self.reward, ennemy.reward
+    
     def get_state(self):
         """
         retourne l'état (des deux joueurs) et du plataeu
         à utiliser dans le dict pour la bsd
         """
         ennemy = self.game.players1 if self == self.game.players2 else self.game.players2
-        return (self.x, self.y, ennemy.x, ennemy.y, self.game.matrix, self.game.board)
+        return (self.x, self.y, ennemy.x, ennemy.y, self.reward, ennemy.reward, self.game.matrix, self.game.board)
+    
+    def exploit(self):
+        best_move = None
+        best_value = float('-inf')
 
-    def reward(self, old_state:tuple, new_state:tuple):
-        """
-        calcule la récompense en fonction de si il prend une case, enclo ou si il bloque l'adversaire
-        +1 : prise case blanche
-        +5 : creation d'enclo
-        -0.5 : par nb de cases blanches autour de l'adversaire (pour le forcer à enfermer l'adversaire)
-        +-20 : victoire/ defaite
-        Returns:
-            float: reaward
-        """
-        reward = 0
-        ennemy = self.game.players1 if self == self.game.players2 else self.game.players2
-
-        if self.game.matrix[new_state[0]][new_state[1]]["color"] == "white":
-            reward += 1
-
-        if self.is_enclosure(new_state[0], new_state[1]):
-            reward += 5
-
-        accessibles_cases = self.accessible_cases(ennemy)
-        reward -= accessibles_cases * 0.5
-
-        if self.game.is_finished():
-            if self.game.get_winner() == self:
-                reward += 20
-            else:
-                reward -= 20
-
-        return reward
-
-    def accessible_cases(self,player):
-        """
-        compte le nb de cases blanches autour d'un joueur
-        Args:
-            player:
-        Returns:
-            int : nb de cases blanches autour du player
-        """
-        count = 0
+        x, y = self.x, self.y
         for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            new_x, new_y = player.x + dx, player.y + dy
-            if (0 <= new_x < self.game.board and 0 <= new_y < self.game.board and
-                    self.game.matrix[new_x][new_y]["color"] == "white"):
-                count += 1
-        return count
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < self.board and 0 <= ny < self.board:  
+                reward = self.q_table.get((nx, ny), 0)  
+                if reward > best_value:
+                    best_value = reward
+                    best_move = (nx, ny)
+        return best_move
 
-    def is_enclosure(self, x: int, y: int) -> bool:
-        """
-        verifie si le déplacement crée un enclo
-        Returns:
-            bool: True si le mouvement crée un encloada
-        """
-        reachable = [[False for _ in range(self.board)] for _ in range(self.board)]
-        queue = [(x, y)]
+    def train(self):
 
-        while queue:
-            cx, cy = queue.pop(0)
-            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                nx, ny = cx + dx, cy + dy
-                if (0 <= nx < self.board and 0 <= ny < self.board and
-                        not reachable[nx][ny] and
-                        self.matrix[nx][ny]["color"] == "white"):
-                    reachable[nx][ny] = True
-                    queue.append((nx, ny))
 
-        return any(self.matrix[i][j]["color"] == "white" and not reachable[i][j]
-                   for i in range(self.board)
-                   for j in range(self.board))
-
-    def play(self) -> str:
-        """
-        return le bind choisi par l'ia, explore ou exploite et initialise l'état si pas encore fait
-        Returns:
-            str : bind
-        """""
-        state = self.get_state()
-        if state not in self.q_table:
-            self.q_table[state] = {
-                "UP": 0, "DOWN": 0, "LEFT": 0, "RIGHT": 0
-            }
-
-        if random.random() < self.eps:
-            return random.choice(["UP", "DOWN", "LEFT", "RIGHT"])
-        else:
-            return max(self.q_table[state].items(), key=lambda x: x[1])[0]
+    
