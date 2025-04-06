f620223e226f577f36970d263f3b23b7b06cb6d1
Hug00at <etu52503@henallux.be>
2025-04-06 16:09:07 +0200

    Update dao.py

--- games/Cube/Game/dao.py
+++ games/Cube/Game/dao.py
@@ -1,19 +1,70 @@
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine, Column, String, Float
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from Game.dico import generate_key
 
 class Base(DeclarativeBase):
-    pass 
+    pass
 
 engine = create_engine('sqlite:///store.db')
-Base.metadata.create_all(engine)
-
+# Création d'une session
 Session = sessionmaker(bind=engine)
-session = Session()
+SESSION = Session()
 
-class Game(Base):
-    __tablename__ = "games"
+class QTable(Base):
+    __tablename__ = 'q_table'
+    unique_key = Column(String, primary_key=True)
+    reward = Column(Float)
 
-    id = Column(Integer, primary_key=True)  # Ajout d'une clé primaire
-    chaine = Column(String)
+    def to_dto(self):
+        return {
+            'unique_key': self.unique_key,
+            'reward': self.reward
+        }
 
+    @classmethod
+    def from_dto(cls, data: dict):
+        return cls(
+            unique_key=data.get('unique_key'),
+            reward=data.get('reward')
+        )
+
+def init_db():
+    # Création des tables
+    Base.metadata.create_all(engine)
+
+def find_all_entries():
+    """
+    Get all entries from the database
+    @return: list of dict
+    """
+    entries = SESSION.query(QTable).all()
+    return [entry.to_dto() for entry in entries]
     
+def find_entry_by_key(unique_key):
+    """
+    Find an entry by unique key
+    @param unique_key: str
+    @return: dict
+    """
+    entry = SESSION.query(QTable).filter(QTable.unique_key == unique_key).first()
+    return entry.to_dto() if entry else None
+
+def save_entry(entry_dto):
+    """
+    Save an entry to the database
+    @param entry_dto: dict
+    """
+    SESSION.add(QTable.from_dto(entry_dto))
+    SESSION.commit()
+
+if __name__ == "__main__":
+    init_db()
+
+    # Exemple d'utilisation
+    unique_key = generate_key('A1', 'B2', 'C3', 'D4', 'matrix_state', 'board_state')
+    entry_dto = {'unique_key': unique_key, 'reward': 0.5}
+    save_entry(entry_dto)
+
+    # Trouver une entrée par clé unique
+    found_entry = find_entry_by_key(unique_key)
+    print(found_entry)
