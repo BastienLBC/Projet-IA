7c0fd43240f25decd04180500fe3031cc43b93a8
Hug00at <etu52503@henallux.be>
2025-04-10 14:27:41 +0200

    Update main.py

--- games/Cube/main.py
+++ games/Cube/main.py
@@ -13,11 +13,6 @@ if __name__ == "__main__":
     board_size = 3
     game_controller = GameController(human, ai2, board_size)
     
-    game_controller.training(ai1, ai2, board_size,800000, epsilon=10)
+    game_controller.training(ai1, ai2, board_size,1000, epsilon=10)
     GameController.compare_ai(ai2)
-    ai2.wins = 0
-    ai2.loses = 0
     
-    
-    game_controller.training(random,ai2,board_size,300 , epsilon=0.1)
-    game_controller.compare_ai(ai2)
