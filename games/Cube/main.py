c56f53369db3076f94e07df5ef1c3b4df200b971
Hug00at <etu52503@henallux.be>
2025-04-14 14:47:50 +0200

    correction

--- games/Cube/main.py
+++ games/Cube/main.py
@@ -5,7 +5,7 @@ from Game.game_controller import GameController
 if __name__ == "__main__":
     init_db()
 
-    # Création des IA
+    
     ai1 = AiPlayer(name="IA_1", color="blue", learning_rate=0.01, gamma=0.9, epsilon=0.9)
     ai2 = AiPlayer(name="IA_2", color="red", learning_rate=0.01, gamma=0.9, epsilon=0.9)
     human = HumanPlayer(name="Joueur 1", color="green")
@@ -13,10 +13,14 @@ if __name__ == "__main__":
     board_size = 3
     nb_games = 5000
     eps = 10
-
+    """
     GameController.training(ai1, ai2, board_size, nb_games, eps)
     print("------------------------------------------------------------")
     GameController.compare_ai(ai1, ai2)
 
     game = GameController(human, ai2, board_size)
+    game.start()
+    """
+
+    game = GameController(ai1, human, board_size)
     game.start()
